e6069f7cacc7703572358163c7a9826f28d7967e
Fernando Warno <ferwar@hotmail.com>
2023-12-26 18:54:17 -0300

    useState - useEffect

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,26 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'Peter',
+    age: 24,
+    hobby: 'coding',
+  });
+
+  const displayPerson = () => {
+    setPerson({ ...person, name: 'John', age: 25, hobby: 'gaming' });
+  };
+
+  return (
+    <>
+      <h2>{person.name}</h2>
+      <h2>{person.age}</h2>
+      <h2>Hobby: {person.hobby}</h2>
+      <button className="btn" onClick={displayPerson}>
+        Show John
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
